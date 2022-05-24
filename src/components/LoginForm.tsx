8347b82990628a775a899e5e08db8be5b3acbdf0
DariaRusovich <rusovichdasha41@gmail.com>
2022-05-24 16:40:42 +0300

    add-yup-refactoring-login-form-with-formik-and-yup

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
+import { Formik } from 'formik';
+import { FC, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { fetchTokens } from '../redux/auth/actions';
 import { authSelector } from '../redux/auth/selectors';
 import { LoginData } from '../types/auth';
+import * as yup from 'yup';
 import Error from './Error';
 import Loader from './Loader';
 
@@ -14,49 +16,89 @@ const LoginForm: FC = () => {
   const [name, setName] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  function handleSubmit(event: React.MouseEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const loginData: LoginData = { username: name, password: password }; 
+  const validationsSchema = yup.object().shape({
+    username: yup.string().required(),
+    password: yup
+      .string()
+      .required('Password is required')
+      .min(4, 'Password is too short - should be 4 chars minimum.')
+      .matches(/[a-zA-Z]/, 'Password must contain Latin letters.'),
+  });
+
+  function handleSubmit(values) {
+    const loginData: LoginData = {
+      username: values.username,
+      password: values.password,
+    };
     dispatch(fetchTokens(loginData));
     navigate('/');
   }
+
   if (loading) {
     return <Loader />;
   }
+
   if (error) {
     return <Error />;
   }
+
   return (
     <section className="section">
       <div className="container form-wrap">
-        <form className="login-form form" onSubmit={handleSubmit}>
-          <fieldset className="title">
-            <legend>Login form</legend>
-            <div className="input-wrapper">
-              <input
-                type="text"
-                name="username"
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Username"
-                required
-              />
-              <div className="validation">*Required</div>
-            </div>
-            <div className="input-wrapper">
-              <input
-                type="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-              />
-              <div className="validation">*Required</div>
-            </div>
-            <button type="submit" className="btn btn-success">
-              Login
-            </button>
-          </fieldset>
-        </form>
+        <Formik
+          initialValues={{ username: '', password: '' }}
+          validateOnBlur
+          onSubmit={(values) => handleSubmit(values)}
+          validationSchema={validationsSchema}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            isValid,
+            handleSubmit,
+            dirty,
+          }) => (
+            <form className="login-form form" onSubmit={handleSubmit}>
+              <fieldset className="title">
+                <legend>Login form</legend>
+                <input
+                  type="text"
+                  name="username"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.username}
+                  placeholder="Username"
+                  required
+                />
+                {touched.username && errors.username && (
+                  <div className="">*{errors.username}</div>
+                )}
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  placeholder="Password"
+                  required
+                />
+                {touched.password && errors.password && (
+                  <div className="validation">*{errors.password}</div>
+                )}
+                <button
+                  type="submit"
+                  className="btn btn-success"
+                  disabled={!isValid && !dirty}
+                >
+                  Login
+                </button>
+              </fieldset>
+            </form>
+          )}
+        </Formik>
       </div>
     </section>
   );
