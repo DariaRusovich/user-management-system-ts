de996a1799f285f9d2c26206c8eba807f2199db0
DariaRusovich <rusovichdasha41@gmail.com>
2022-05-28 17:45:41 +0300

    add-department-name-to-the-employee-list

--- src/components/EmployeesList.tsx
+++ src/components/EmployeesList.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 import AddEmployeeForm from '../modalForms/AddEmployeeForm';
+import { fetchDepartment } from '../redux/departments/actions';
+import { departmentsSelector } from '../redux/departments/selectors';
 import { fetchEmployees } from '../redux/employees/actions';
 import { employeesSelector } from '../redux/employees/selectors';
 import Employee from './Employee';
@@ -10,11 +12,13 @@ import Loader from './Loader';
 
 const EmployeesList = () => {
   const { employees, error, loading } = useSelector(employeesSelector);
+  const { department } = useSelector(departmentsSelector);
   const { id } = useParams() as { id: string };
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchEmployees(id));
+    dispatch(fetchDepartment(id));
   }, [id]);
 
   // if (error) {
@@ -30,7 +34,7 @@ const EmployeesList = () => {
       <div className="container section-wrap">
         <AddEmployeeForm id={id}></AddEmployeeForm>
         <h1 className="title-secondary">
-          {employees.length} employees.
+          {employees.length} employees in the <span>{department.name}</span> department <br />
           <Link to="/" className="title">
             Go back
           </Link>
